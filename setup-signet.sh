9a4c89a3a4fb45251eb952d9926d67a3dcd1f324
benthecarman <benthecarman@live.com>
2023-04-06 15:38:36 -0500

    Make script work for descriptor wallets

--- setup-signet.sh
+++ setup-signet.sh
@@ -1,14 +1,14 @@
 PRIVKEY=${PRIVKEY:-$(cat ~/.bitcoin/PRIVKEY.txt)}
 DATADIR=${DATADIR:-~/.bitcoin/}
 bitcoind -datadir=$DATADIR --daemonwait -persistmempool
-bitcoin-cli -datadir=$DATADIR -named createwallet wallet_name="custom_signet" load_on_startup=true descriptors=false
+bitcoin-cli -datadir=$DATADIR -named createwallet wallet_name="custom_signet" load_on_startup=true
 
 #only used in case of mining node
 if [[ "$MINERENABLED" == "1" ]]; then
-    bitcoin-cli -datadir=$DATADIR importprivkey $PRIVKEY
-    ## for future with descriptor wallets, cannot seem to get it working yet
-    # descinfo=$(bitcoin-cli getdescriptorinfo "wpkh(${PRIVKEY})")
-    # checksum=$(echo "$descinfo" | jq .checksum | tr -d '"' | tr -d "\n")
-    # desc='[{"desc":"wpkh('$PRIVKEY')#'$checksum'","timestamp":0,"internal":false}]'
-    # bitcoin-cli -datadir=$DATADIR importdescriptors $desc
+  echo "Descriptor = wpkh(${PRIVKEY})"
+  descinfo=$(bitcoin-cli getdescriptorinfo "wpkh(${PRIVKEY})")
+  checksum=$(echo "$descinfo" | jq .checksum | tr -d '"' | tr -d "\n")
+  desc='[{"desc":"wpkh('$PRIVKEY')#'$checksum'","timestamp":0,"internal":false}]'
+  echo "Importing descriptor: $desc"
+  bitcoin-cli -datadir=$DATADIR importdescriptors $desc
 fi
